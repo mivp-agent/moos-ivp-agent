94953c9c23b6c0f3a047fe5f589bf35b90b61f1f
carter.fendley <carter.fendley@gmail.com>
2021-07-20 22:52:43 -0400

    Inital Commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM moosivp/moos-ivp:r9177-gui
+LABEL maintainer = Michael Misha Novitzky <michael.novitzky@westpoint.edu>
+
+ENV MOOS="moos-ivp-aquaticus"
+ENV PATH="/home/moos/${MOOS}/bin:${PATH}"
+ENV IVP_BEHAVIOR_DIRS="${IVP_BEHAVIOR_DIRS}:/home/moos/${MOOS}/lib"
+
+# System Dependencies
+USER root
+RUN usermod -aG sudo moos
+RUN echo "moos:moos" | chpasswd
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y libncurses-dev sudo python2.7-dev python-pip python-tk && apt-get clean
+RUN apt-get install -y vim emacs-nox tmux git
+RUN pip2 install --no-cache-dir numpy matplotlib 'tensorflow==1.5' 'keras==2.0.8' colorama h5py
+USER moos
+
+# Aquaticus tree
+RUN svn co "https://oceanai.mit.edu/svn/${MOOS}-oai/trunk/" "${HOME}/${MOOS}"
+RUN cd "${HOME}/${MOOS}" && ./build.sh
+
+# RLquaticus
+RUN mkdir -p moos-ivp-RLquaticus/missions
+
+# pLearn tree
+#ENV PLEARN="moos-ivp-pLearn"
+#ENV PATH="/home/moos/${PLEARN}/bin:${PATH}"
+#ENV IVP_BEHAVIOR_DIRS="/home/moos/${PLEARN}/lib:${IVP_BEHAVIOR_DIRS}"
+#ENV PYTHONPATH="${PYTHONPATH}:/home/moos/${PLEARN}/pLearn/learning_code:/home/moos/${PLEARN}/src/lib_python"
+
+#RUN git clone https://github.com/mnovitzky/moos-ivp-pLearn.git
+#COPY --chown=moos:moos . moos-ivp-pLearn/
+
+#RUN cd ${PLEARN} && ./build.sh
---
+++ docker.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Cd to dir to make paths uniform
+DIRNAME="$(dirname $0)"
+cd $DIRNAME
+
+# Test OS type for use letter
+OS_TYPE="UNSET"
+
+if [ "$(uname)" == "Linux" ]; then
+    OS_TYPE="linux"
+elif [ "$(uname)" == "Darwin" ]; then
+    OS_TYPE="osx"
+else
+    printf "ERROR: Unable to determine OS type.\n"
+    exit 1
+fi
+
+
+# Handle arguments
+if [[ -z "$1" ]] || [[ "$1" = "help" ]] || [[ "$1" = "--help" ]] || [[ "$1" = "-h" ]]; then
+    printf "Usage: %s <COMMAND>\n" $0
+    printf "Commands:\n"
+    printf "\t build   - Build docker container and tag as \"RLquaticus\"\n"
+    printf "\t run     - Run a docker container from image tagged as \"RLquaticus\"\n"
+    printf "\t stop    - Stops a docker container from image tagged as \"RLquaticus\"\n"
+    printf "\t rm      - Removes a docker container from image tagged as \"RLquaticus\"\n"
+    printf "\t connect - Connect to the docker containertagged ad \"RLquaticus\"\n"
+    exit 0;
+elif [[ "$1" == "build" ]]; then
+    printf "Building plearn container...\n"
+    docker build -t rl-quaticus:1.0 .
+elif [[ "$1" == "run" ]]; then
+    printf "Enabling xhost server...\n"
+    xhost +
+    printf "Starting docker container...\n"
+    printf "\n==========================================\n"
+    printf "= To exit and stop: run command \"exit\"   =\n"
+    printf "= To detach: CTRL+p CTRL+q               =\n"
+    printf "==========================================\n\n"
+
+    if [[ "$OS_TYPE" == "osx" ]]; then
+        docker run --env="DISPLAY=host.docker.internal:0" \
+            --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+            --mount type=bind,source="$(pwd)"/missions,target=/home/moos/moos-ivp-RLquaticus/missions \
+            --name rl-quaticus -it rl-quaticus:1.0 bash
+    elif [[ "$OS_TYPE" == "linux" ]]; then
+        docker run --env="DISPLAY" \
+            --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+            --mount type=bind,source="$(pwd)"/missions,target=/home/moos/moos-ivp-RLquaticus/missions \
+            --name rl-quaticus -it rl-quaticus:1.0 bash
+    fi
+    printf "WARNING: Docker container will run in background unless stopped\n"
+elif [[ "$1" == "connect" ]]; then
+    printf "Conecting to docker container...\n"
+    printf "\n==========================================\n"
+    printf "= To detach: CTRL+p CTRL+q               =\n"
+    printf "==========================================\n\n"
+    docker exec -it rl-quaticus bash
+elif [[ "$1" == "stop" ]]; then
+    printf "Stopping docker container...\n"
+    docker stop rl-quaticus
+elif [[ "$1" == "rm" ]]; then
+    printf "Deleting docker container...\n"
+    docker rm rl-quaticus
+else
+    printf "Error: Unrecognized argument\n"
+    exit;
+fi
