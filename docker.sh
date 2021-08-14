cc2cbfdd94bf126e6f39f72f5ca5f34e288ae227
carter.fendley <carter.fendley@gmail.com>
2021-08-14 18:11:27 -0400

    Add ability to launch multiple docker containers

--- docker.sh
+++ docker.sh
@@ -16,6 +16,12 @@ else
     exit 1
 fi
 
+NAME="mivp-agent"
+if [[ -n "$2" ]]; then
+    printf "Settting name to $2...\n"
+    NAME="$2"
+fi
+
 # Handle arguments
 if [[ -z "$1" ]] || [[ "$1" = "help" ]] || [[ "$1" = "--help" ]] || [[ "$1" = "-h" ]]; then
     printf "Usage: %s <COMMAND>\n" $0
@@ -28,7 +34,7 @@ if [[ -z "$1" ]] || [[ "$1" = "help" ]] || [[ "$1" = "--help" ]] || [[ "$1" = "-
     exit 0;
 elif [[ "$1" == "build" ]]; then
     printf "Building mivp_agent container...\n"
-    docker build -t mivp-agent:1.0 .
+    docker build -t "$NAME:1.0" .
 elif [[ "$1" == "run" ]]; then
     printf "Enabling xhost server...\n"
     xhost +
@@ -45,7 +51,7 @@ elif [[ "$1" == "run" ]]; then
             --mount type=bind,source="$(pwd)"/src,target=/home/moos/moos-ivp-agent/src \
             --mount type=bind,source="$(pwd)"/examples,target=/home/moos/moos-ivp-agent/examples \
             --workdir="/home/moos/moos-ivp-agent" \
-            --name mivp-agent -it mivp-agent:1.0 bash
+            --name "$NAME" -it "$NAME:1.0" bash
     elif [[ "$OS_TYPE" == "linux" ]]; then
         docker run --env="DISPLAY" \
             --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
@@ -53,7 +59,7 @@ elif [[ "$1" == "run" ]]; then
             --mount type=bind,source="$(pwd)"/src,target=/home/moos/moos-ivp-agent/src \
             --mount type=bind,source="$(pwd)"/examples,target=/home/moos/moos-ivp-agent/examples \
             --workdir="/home/moos/moos-ivp-agent" \
-            --name mivp-agent -it mivp-agent:1.0 bash
+            --name "$NAME" -it "$NAME:1.0" bash
     fi
     printf "WARNING: Docker container will run in background unless stopped\n"
 elif [[ "$1" == "connect" ]]; then
@@ -61,13 +67,13 @@ elif [[ "$1" == "connect" ]]; then
     printf "\n==========================================\n"
     printf "= To detach: CTRL+p CTRL+q               =\n"
     printf "==========================================\n\n"
-    docker exec -it mivp-agent bash
+    docker exec -it "$NAME" bash
 elif [[ "$1" == "stop" ]]; then
     printf "Stopping docker container...\n"
-    docker stop mivp-agent
+    docker stop "$NAME"
 elif [[ "$1" == "rm" ]]; then
     printf "Deleting docker container...\n"
-    docker rm mivp-agent
+    docker rm "$NAME"
 else
     printf "Error: Unrecognized argument\n"
     exit;
