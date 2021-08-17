3462c2ab4faf878755e82e4e1df648a33652f167
carter.fendley <carter.fendley@gmail.com>
2021-08-17 11:41:38 -0400

    Change launch_pair to work with new can_tags

--- examples/QTable/mission/launch_pair.sh
+++ examples/QTable/mission/launch_pair.sh
@@ -41,10 +41,10 @@ fi
 if [[ -z $NO_HERON ]]; then
   cd ./heron
   # Launch a red agent
-  ./launch_heron.sh red agent 11 --behavior=ATTACK_LEFT --color=orange $TIME_WARP > /dev/null &
+  ./launch_heron.sh red agent 11 --behavior=ATTACK --color=orange $TIME_WARP > /dev/null &
   sleep 1
   # Launch a blue drone
-  #./launch_heron.sh blue drone 12 --behavior=DEFEND --color=orange $TIME_WARP > /dev/null &
+  ./launch_heron.sh blue drone 21 --behavior=DEFEND --color=orange $TIME_WARP > /dev/null &
   sleep 1
   cd ..
 fi
