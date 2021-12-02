9eb41ab9449d28fd9241f863ed75cc61d834788a
carter.fendley <carter.fendley@gmail.com>
2021-12-02 15:43:16 -0500

    Remove old proto test method

--- src/python_module/test/test_proto.py
+++ src/python_module/test/test_proto.py
@@ -85,38 +85,6 @@ class TestProto(unittest.TestCase):
     state['NODE_REPORTS']['henry']['MOOS_TIME'] = 0.123
     self.do_state_translate_test(state)
 
-  def not_test_basic(self):
-    # The following test that we are able to serialize
-
-    report = moos_pb2.NodeReport()
-    self.assertRaises(EncodeError, report.SerializeToString)
-
-    report.vname = "agent"
-    report.NAV_X = 40.1
-    report.NAV_Y = -50.4
-    report.NAV_HEADING = 143.2
-    report.SerializeToString()
-
-    aquaticus = aquaticus_vehicle_pb2.AquaticusVehicle()
-    self.assertRaises(EncodeError, aquaticus.SerializeToString)
-
-    aquaticus.tagged = False
-    aquaticus.has_flag = True
-    aquaticus.node_report.CopyFrom(report)
-    aquaticus.SerializeToString()
-
-    agent = agent_state_pb2.AgentState()  
-    self.assertRaises(EncodeError, agent.SerializeToString)
-    agent.MOOS_TIME = 0.0
-    agent.vehicle.CopyFrom(aquaticus)
-    
-    other_report1 = agent.other_reports.add()
-    other_report1.CopyFrom(report)
-
-    other_report2 = agent.other_reports.add()
-    other_report2.CopyFrom(report)
-
-    agent.SerializeToString()
 
 from mivp_agent.proto.proto_logger import ProtoLogger
 
